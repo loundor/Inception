bec9859391291179631542ea3f05e4127a6f36fa
Loundor <loundor@gmail.com>
2023-01-11 15:00:57 +0100

    Delete srcs directory

--- srcs/requirements/mariadb/conf/createdb.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE Base;
-CREATE USER 'stissera'@'%' IDENTIFIED BY 'qwerty';
-GRANT ALL PRIVILEGES ON Base.* TO 'stissera'@'%';
-FLUSH PRIVILEGES;
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'qwerty';
